82960cc9ccd27d70f00575f431b0c14801013db5
MajaS301 <maja5steler@gmail.com>
2024-12-09 19:18:03 +0100

    promjena tablica

--- pohadjanje_vrtica.sql
+++ pohadjanje_vrtica.sql
@@ -8,43 +8,33 @@ use pohadjanje_Vrtica;
 go
 
 create table skupine(
-sifra int not null identity(1,1),
-naziv varchar(20) not null primary key,
+sifra int not null primary key identity(1,1),
+naziv varchar(20) not null,
 prostorija varchar(20)
 );
 
-create table podskupine(
-sifra int not null identity(1,1),
-naziv varchar(50) not null primary key,
-skupina varchar(20) not null references skupine(naziv),
-teta varchar(50) not null
-);
-
 create table tete(
-sifra int not null identity(1,1),
+sifra int not null primary key identity(1,1),
 ime varchar(50) not null,
 prezime varchar(50) not null,
-podskupina varchar(50) not null references podskupine(naziv),
 broj_mobitela varchar(20),
 oib char(11)
 );
 
+create table podskupine(
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null,
+skupina int not null references skupine(sifra),
+teta int not null references tete(sifra)
+);
+
+
 create table djeca(
 sifra int not null identity(1,1),
-podskupina varchar(50) not null references podskupine(naziv),
+podskupina int not null references podskupine(sifra),
 ime varchar(20) not null primary key,
 prezime varchar(20) not null,
 spol varchar (6) not null,
 datum_rodjenja date not null,
 kontakt_roditelja varchar (20) not null
-);
-
-
-create table roditelji(
-sifra int not null identity(1,1),
-ime varchar(20) not null,
-prezime varchar(20) not null,
-djeca varchar(20) not null references djeca(ime),
-broj_mobitela varchar(20),
-email varchar(50)
 );
