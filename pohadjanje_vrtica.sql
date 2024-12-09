0615aebf6ee62ec54095326d6445e95e5ded1414
MajaS301 <maja5steler@gmail.com>
2024-12-09 20:20:45 +0100

    insert into tete

--- pohadjanje_vrtica.sql
+++ pohadjanje_vrtica.sql
@@ -1,20 +1,20 @@
 ﻿use master
 go
-drop database if exists pohađanje_Vrtića;
+drop database if exists pohadjanje_Vrtica;
 go
-create database pohađanje_Vrtića;
+create database pohadjanje_Vrtica;
 go
-use pohađanje_Vrtića;
+use pohadjanje_Vrtica;
 go
 
 create table skupine(
-šifra int not null primary key identity(1,1),
+sifra int not null primary key identity(1,1),
 naziv varchar(20) not null,
 prostorija varchar(20) not null
 );
 
 create table tete(
-šifra int not null primary key identity(1,1),
+sifra int not null primary key identity(1,1),
 ime varchar(50) not null,
 prezime varchar(50) not null,
 broj_mobitela varchar(20),
@@ -22,21 +22,21 @@ oib char(11)
 );
 
 create table podskupine(
-šifra int not null primary key identity(1,1),
+sifra int not null primary key identity(1,1),
 naziv varchar(50) not null,
-skupina int not null references skupine(šifra),
-teta int not null references tete(šifra)
+skupina int not null references skupine(sifra),
+teta int not null references tete(sifra)
 );
 
 
 create table djeca(
-šifra int not null primary key identity(1,1),
-podskupina int not null references podskupine(šifra),
+sifra int not null primary key identity(1,1),
+podskupina int not null references podskupine(sifra),
 ime varchar(20) not null,
 prezime varchar(20) not null,
 spol bit not null,
-datum_rođenja date not null,
-kontakt_roditelja varchar (20) not null
+datum_rodjenja date,
+kontakt_roditelja varchar (20)
 );
 
 
@@ -46,3 +46,12 @@ insert into skupine (naziv, prostorija) values
 insert into skupine (naziv, prostorija) values
 ('Točkice' , 'soba 129');
 
+
+insert into tete (ime, prezime) values
+('Ana Marija' , 'Novak')
+insert into tete (ime, prezime) values
+('Lara', 'Petrović')
+insert into tete (ime, prezime) values
+('Ema', 'Vukić')
+insert into tete (ime, prezime) values
+('Sofija', 'Pavlović')
